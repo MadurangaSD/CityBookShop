8efba923b35f6b75d5a41076eef91d2a403e7b3c
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-02-20 23:39:09 +0530

    Update Book.java

--- src/model/Book.java
+++ src/model/Book.java
@@ -5,16 +5,30 @@ public class Book {
     private String name;
     private double price;
     private String category;
+    private int quantity;
 
-    public Book(String name, double price, String category) {
+    public Book(String name, double price, String category, int quantity) {
         this.name = name;
         this.price = price;
         this.category = category;
+        this.quantity = quantity;
     }
 
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public String getCategory() { return category; }
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
 
     @Override
     public String toString() {
